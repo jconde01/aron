e92a520c87616b1554a52f6ceab34ead71a52af8
maestro362 <40808304+maestro362@users.noreply.github.com>
2019-04-02 15:22:26 -0600

    captura eerores documentos

--- resources/views/consultas/documentos/documentos.blade.php
+++ resources/views/consultas/documentos/documentos.blade.php
@@ -11,15 +11,20 @@
 	 	
 	
 		    <?php
-            $directorio = opendir($ruta_documentos); 
-            while ($archivo = readdir($directorio)) //obtenemos un archivo y luego otro sucesivamente
-            {
-                if (!is_dir($archivo))//verificamos si es o no un directorio
+            try {
+                $directorio = opendir($ruta_documentos); 
+                while ($archivo = readdir($directorio)) //obtenemos un archivo y luego otro sucesivamente
                 {
-                    echo '<div class="col-lg-5" style="border:1px blue solid; border-radius:10px; margin-right: 80px; height:170px; margin-top: 20px;" > <br> <i class="fas fa-file-alt" style="font-size: 50px;" > </i> <br> <br> <a href="/consultas/descargaDocumentos/'.$subcarpeta.'/'.$archivo.'" > '.$archivo.' </a><br/><br><br>';
-                    echo '</div>';
+                    if (!is_dir($archivo))//verificamos si es o no un directorio
+                    {
+                        echo '<div class="col-lg-5" style="border:1px blue solid; border-radius:10px; margin-right: 80px; height:170px; margin-top: 20px;" > <br> <i class="fas fa-file-alt" style="font-size: 50px;" > </i> <br> <br> <a href="/consultas/descargaDocumentos/'.$subcarpeta.'/'.$archivo.'" > '.$archivo.' </a><br/><br><br>';
+                        echo '</div>';
+                    }
                 }
+            } catch (Exception $e) {
+                echo '<span style="color: white;">'.$e->getMessage().'</span>';
             }
+            
         ?>               
         <br>
       </div>
